019d9dbcd0d38f11485d2aba31581bf4e02b201d
Oborin Nikita <oborinikita@yandex.ru>
2023-09-04 22:21:45 +0700

    feat: add maven project

---
+++ src/main/java/com/project/movieratingapp/MovieRatingAppApplication.java
@@ -0,0 +1,13 @@
+package com.project.movieratingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieRatingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieRatingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/movieratingapp/MovieRatingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.movieratingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieRatingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
